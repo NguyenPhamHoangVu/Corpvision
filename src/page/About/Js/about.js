b2597d9a3d080a736b32dbc161450d0ae5a24fdd
NguyenPhamHoangVu <hoangvucv1@gmail.com>
2024-06-10 00:29:28 +0700

    push code

---
+++ src/page/About/Js/about.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var header = document.querySelector(".header");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 0) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+});
